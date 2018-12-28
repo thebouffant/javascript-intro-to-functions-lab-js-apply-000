a172b4488ce73c60db37a4b1cbacba454107f82a
Marc <marc.troughton@gmail.com>
2018-12-28 14:41:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  console.log (string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log (string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  
+  return string.toUpperCase()
+}
