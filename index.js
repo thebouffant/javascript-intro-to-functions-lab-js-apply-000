f5648a2608dfe3828f329644502f8bde6b6161b2
Marc <marc.troughton@gmail.com>
2018-12-28 14:46:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()){
     return "YES INDEED!"
-  } else if {
-    
-  }
-  
-  return string.toUpperCase()
+  } else if(string === string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if (string === "I love you, Grandma.")
+    return 
 }
